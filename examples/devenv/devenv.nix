c6eee439b52f589228abc0d96847d9d054cda367
Yves Parès <yves@lili.ai>
2024-09-16 17:47:23 +0200

    Updated devenv example

--- examples/devenv/devenv.nix
+++ examples/devenv/devenv.nix
@@ -6,6 +6,7 @@ let
   myNushell = inputs.nushellWith.lib.nushellWith {
     inherit pkgs;
     from-nix = { nu_plugin_polars = pkgs.nushellPlugins.polars; };
+    from-source = { nu_plugin_highlight = inputs.highlight; };
   };
 in {
   packages = [ myNushell ];
