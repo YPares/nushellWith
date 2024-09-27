27607aab18f100967dc5a14d050d8b40a71376cc
Ywen <yves.pares@gmail.com>
2024-09-27 23:49:45 +0200

    Update nu-libraries.nix

--- nu-libraries.nix
+++ nu-libraries.nix
@@ -3,15 +3,13 @@
 # When adding a library here, don't forget to add its inputs to the main flake.nix
 { makeNuLibrary, pkgs, ... }@inputs: {
 
-  # https://github.com/Jan9103/webserver.nu"
-  webserver-nu = makeNuLibrary {
+  webserver-nu = makeNuLibrary { # https://github.com/Jan9103/webserver.nu"
     inherit pkgs;
     name = "webserver-nu";
     src = inputs.webserver-nu-src;
     path = with pkgs; [ "${netcat}/bin" "${coreutils}/bin" ];
   };
-
-  nu-batteries = makeNuLibrary {
+  nu-batteries = makeNuLibrary { # https://github.com/nome/nu-batteries
     inherit pkgs;
     name = "nu-batteries";
     src = inputs.nu-batteries-src;
