618b6809d20795ea6063e141647a503cf0bba6f4
Yves Parès <yves@lili.ai>
2024-10-24 09:19:06 +0200

    Reorganized code in nu-libs-and-plugins.nix

--- nix-src/nu-libs-and-plugins.nix
+++ nix-src/nu-libs-and-plugins.nix
@@ -4,69 +4,70 @@
 { self, pkgs, ... }@inputs:
 
 let
-  # Shortcut to build a nu library without too much fuss:
-  simpleLib = name: extraArgs:
-    self.lib.makeNuLibrary ({
-      inherit pkgs name;
-      src = inputs."${name}-src";
-    } // extraArgs);
 
-  craneLib = inputs.crane.mkLib pkgs;
+  nu-libraries = let # Shortcut to build a nu library without too much fuss:
+    simpleNuLib = name: extraArgs:
+      self.lib.makeNuLibrary ({
+        inherit pkgs name;
+        src = inputs."${name}-src";
+      } // extraArgs);
+  in {
 
-  cratesIoJsonIndex = self.lib.runNuScript pkgs "plugins-in-crates.io-index"
-    ../nu-src/list-plugins-in-index.nu [ inputs.crates-io-index ];
-
-  cratesIoIndex =
-    builtins.fromJSON (builtins.readFile "${cratesIoJsonIndex}/plugins.json");
-
-  baseBuildInputs = with pkgs;
-    [ pkg-config ] ++ lib.optionals (stdenv.hostPlatform.isDarwin) [
-      iconv
-      darwin.apple_sdk.frameworks.IOKit
-      darwin.apple_sdk.frameworks.Security
-    ];
-
-  buildPluginFromCratesIo = { name, ... }@nameVerCksum:
-    let
-      src = craneLib.downloadCargoPackage (nameVerCksum // {
-        source = "registry+https://github.com/rust-lang/crates.io-index";
-      });
-      buildInputs = baseBuildInputs
-        ++ (buildInputsForPluginsFromCratesIo.${name} or [ ]);
-      cargoArtifacts = craneLib.buildDepsOnly { inherit src buildInputs; };
-    in craneLib.buildPackage {
-      inherit src buildInputs cargoArtifacts;
-      doCheck = false;
+    nu-batteries = simpleNuLib "nu-batteries" { };
+    webserver-nu = simpleNuLib "webserver-nu" {
+      path = with pkgs; [ "${netcat}/bin" "${coreutils}/bin" ];
     };
 
-  # Non-rust dependencies for plugins from crates.io
-  buildInputsForPluginsFromCratesIo = with pkgs; {
-    nu_plugin_plotters = [ fontconfig ];
-    nu_plugin_query = [ openssl ];
   };
 
-  # An attrset of all the plugins from crates.io
-  # Each attr is of "nu_plugin_<name>"
-  pluginsFromCratesIo =
-    builtins.mapAttrs (_: buildPluginFromCratesIo) cratesIoIndex;
+  nu-plugins = let
+    craneLib = inputs.crane.mkLib pkgs;
 
-in pluginsFromCratesIo // {
+    cratesIoJsonIndex = self.lib.runNuScript pkgs "plugins-in-crates.io-index"
+      ../nu-src/list-plugins-in-index.nu [ inputs.crates-io-index ];
 
-  # Libraries (nu code) from github
+    cratesIoIndex =
+      builtins.fromJSON (builtins.readFile "${cratesIoJsonIndex}/plugins.json");
 
-  nu-batteries = simpleLib "nu-batteries" { };
-  webserver-nu = simpleLib "webserver-nu" {
-    path = with pkgs; [ "${netcat}/bin" "${coreutils}/bin" ];
-  };
+    pluginsBaseBuildInputs = with pkgs;
+      [ pkg-config ] ++ lib.optionals (stdenv.hostPlatform.isDarwin) [
+        iconv
+        darwin.apple_sdk.frameworks.IOKit
+        darwin.apple_sdk.frameworks.Security
+      ];
 
-  # Plugins (rust code) from github 
+    # Non-rust dependencies for plugins from crates.io
+    #
+    # Deps are to be added here on a case-by-case fashion
+    buildInputsForPluginsFromCratesIo = with pkgs; {
+      nu_plugin_plotters = [ fontconfig ];
+      nu_plugin_query = [ openssl ];
+    };
 
-  nu_plugin_httpserve =
-    let src = craneLib.cleanCargoSource inputs.nu_plugin_httpserve-src;
-        buildInputs = baseBuildInputs;
+    buildPluginFromCratesIo = { name, ... }@nameVerCksum:
+      let
+        src = craneLib.downloadCargoPackage (nameVerCksum // {
+          source = "registry+https://github.com/rust-lang/crates.io-index";
+        });
+        buildInputs = pluginsBaseBuildInputs
+          ++ (buildInputsForPluginsFromCratesIo.${name} or [ ]);
         cargoArtifacts = craneLib.buildDepsOnly { inherit src buildInputs; };
-    in craneLib.buildPackage {
-      inherit src buildInputs cargoArtifacts;
+      in craneLib.buildPackage {
+        inherit src buildInputs cargoArtifacts;
+        doCheck = false;
+      };
+
+  in ( # All the plugins from crates.io:
+    # (Each attr is of the form "nu_plugin_<name>")
+    builtins.mapAttrs (_: buildPluginFromCratesIo) cratesIoIndex) // {
+      # Nu plugins from sources other than crates.io:
+
+      nu_plugin_httpserve = let
+        src = craneLib.cleanCargoSource inputs.nu_plugin_httpserve-src;
+        buildInputs = pluginsBaseBuildInputs;
+        cargoArtifacts = craneLib.buildDepsOnly { inherit src buildInputs; };
+      in craneLib.buildPackage { inherit src buildInputs cargoArtifacts; };
+
     };
 
-}
+in nu-libraries // nu-plugins
