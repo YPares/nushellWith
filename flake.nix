37ebca1e695f89a1a3df86914d93b4b6788ef928
Yves Pares <yves.pares@aletiq.com>
2025-05-05 23:08:47 +0200

    Added a check for each packaged plugin
    This check tests whether a nushell instance can be started correctly with this plugin

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@
       lib = import ./nix-src/lib.nix flake-inputs;
 
       # Makes the flake directly usable as a function:
-      __functor = (_: self.lib.nushellWith);
+      __functor = _: self.lib.nushellWith;
 
       packages = nixpkgs.lib.genAttrs supported-systems (
         system:
@@ -90,22 +90,45 @@
         in
         nu-libs-and-plugins
         // (with nu-libs-and-plugins; {
-          nushell = nu-with "nushell-raw" [ ] [ ];
+          nushellRaw = nu-with "nushell-raw" [ ] [ ];
           nushellWithStdPlugins = nu-with "nushell-with-std-plugins" [ ] std-plugins;
-          nushellWithExtras =
-            nu-with "nushell-with-extras"
-              [
-                nu-batteries
-              ]
-              (
-                std-plugins
-                ++ [
-                  nu_plugin_file
-                  nu_plugin_plotters
-                  nu_plugin_vec
-                ]
-              );
+          nushellWithExtras = nu-with "nushell-with-extras" [ nu-batteries ] (
+            std-plugins
+            ++ [
+              nu_plugin_file
+            ]
+          );
         })
+      );
+
+      # For each plugin, check that it can be added to nushell without errors:
+      checks = nixpkgs.lib.genAttrs supported-systems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          all-plugin-names =
+            with builtins;
+            filter (name: pkgs.lib.strings.hasPrefix "nu_plugin_" name) (attrNames self.packages.${system});
+        in
+        nixpkgs.lib.genAttrs all-plugin-names (
+          plugin-name:
+          let
+            nu = self.lib.nushellWith {
+              inherit pkgs;
+              name = "nu-with-${plugin-name}";
+              plugins.nix = [ self.packages.${system}.${plugin-name} ];
+            };
+          in
+          pkgs.runCommand "check_${plugin-name}" { } ''
+            ${nu}/bin/nu -c '
+              if ((plugin list).status.0 == "running") {
+                touch $env.out
+              } else {
+                error make {msg: "${plugin-name} not running"}
+              }
+            '
+          ''
+        )
       );
     };
 }
