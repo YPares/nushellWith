93c256fb81202589ca468815ac12893976f0c8e6
Yves Pares <yves.pares@aletiq.com>
2025-06-06 07:35:51 +0000

    Updated flake.lock and crates-io-source

--- crates-io-source.nix
+++ crates-io-source.nix
@@ -1,6 +1,6 @@
 builtins.fetchGit {
   url = "https://github.com/rust-lang/crates.io-index";
   ref = "master";
-  rev = "da58632017d931c816ff679848fc6e9bf9740338";
-  narHash = "sha256-pTX39D0BMe1V1ntjjPTZpKSpnCxLjXcOHAjrcvTTNmg=";
+  rev = "d3ec3d4a1682cf8e2815a2855835bbecd413745b";
+  narHash = "sha256-lWVnzjz7Y1SB3M9PKKJGnZA076TMv1qd4YFxK0yT26w=";
 }
