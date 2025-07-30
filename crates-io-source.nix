6dfa808a0630594813cc0bb3a22478c809425fb7
Yves Pares <yves.pares@aletiq.com>
2025-07-30 14:14:06 +0200

    Updated to nushell 0.106.1

--- crates-io-source.nix
+++ crates-io-source.nix
@@ -1,6 +1,6 @@
 builtins.fetchGit {
   url = "https://github.com/rust-lang/crates.io-index";
   ref = "master";
-  rev = "8514b7cc847a1367a4b50dc1f9b7d605b8b307a9";
-  narHash = "sha256-eylMmybfQpe/icM8aLK4EzJXK3G+R3pVoklIYT4qS7U=";
+  rev = "e82926474baf804eebd4bc116a0becf5e871ca9f";
+  narHash = "sha256-CwDtiJWN2QVvaiZVGBt/KPUbimxKyTqhclVxKjYjSVw=";
 }
