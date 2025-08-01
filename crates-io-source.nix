1518eceac0dbe16d2a22fce919f254b27e3863fd
Yves Pares <yves.pares@aletiq.com>
2025-08-01 19:57:06 +0200

    Updated crates-io index

--- crates-io-source.nix
+++ crates-io-source.nix
@@ -1,6 +1,6 @@
 builtins.fetchGit {
   url = "https://github.com/rust-lang/crates.io-index";
   ref = "master";
-  rev = "e82926474baf804eebd4bc116a0becf5e871ca9f";
-  narHash = "sha256-CwDtiJWN2QVvaiZVGBt/KPUbimxKyTqhclVxKjYjSVw=";
+  rev = "f6c3c5d1b7adbba7156096ba8ea11f7df1189b6d";
+  narHash = "sha256-SphS3Iw5j5ScMscJRyMISTK7uHfVA1fZE9CeY9xwJuc=";
 }
