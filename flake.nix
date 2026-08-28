1de5b15e2628860e8f044bf5c83249e026fe8003
Yves Pares <yves.pares@aletiq.com>
2026-08-28 17:41:34 +0200

    Updated to nu0.115.1

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     # Latest Nushell stable version:
     nushell-src = {
-      url = "github:nushell/nushell/0.115.0";
+      url = "github:nushell/nushell/0.115.1";
       flake = false;
     };
 
